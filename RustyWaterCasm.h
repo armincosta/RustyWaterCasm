f6fd3e90c2e5b0f19d0bb9cb030d16412ef02613
Costa Armin <armin.costa@eurac.edu>
2020-12-18 21:14:50 +0100

    fix wrong name

--- RustyWaterCasm.h
+++ RustyWaterCasm.h
@@ -15,7 +15,7 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Kent_display_serial_protocol_25016 is distributed in the hope that it will be useful,
+# RustyWaterCasm is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
